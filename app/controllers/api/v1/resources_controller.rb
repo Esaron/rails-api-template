2fc2c264781f15ee9f8f920ae4f0749190180dfb
Jon Marshall <jon.marshall@betterup.co>
2023-02-25 12:38:30 -0500

    Implement CRUD methods

--- app/controllers/api/v1/resources_controller.rb
+++ app/controllers/api/v1/resources_controller.rb
@@ -1,6 +1,37 @@
 class Api::V1::ResourcesController < ApplicationController
+  before_action :find_resource, only: [:show, :update]
+
   def index
-    @resource = Resource.all
+    @resources = Resource.all
     render json: @resources
+  end
+
+  def show
+    render json: @resource
+  end
+
+  def create
+    @resource = Resource.create!(upsert_params)
+    render json: @resource
+  end
+
+  def update
+    @resource.update!(upsert_params)
+    render json: @resource
+  end
+
+  def destroy
+    Resource.destroy_by(params[:id])
+    head :no_content
+  end
+
+  private
+
+  def find_resource
+    @resource = Resource.find(params[:id])
+  end
+
+  def upsert_params
+    params.permit(:name)
   end
 end
